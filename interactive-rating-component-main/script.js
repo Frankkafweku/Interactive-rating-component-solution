7940f692909d8968bc7489a867eee6155b22a8f3
Frank Kafweku <frankkafweku84@gmail.com>
2023-08-31 18:29:27 +0200

    Finish the sucsess page

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -5,26 +5,41 @@ const rate3 = document.getElementById("rate-3")
 const rate4 = document.getElementById("rate-4")
 const rate5 = document.getElementById("rate-5")
 
+
+// variable for submit button
+const submitBtn = document.getElementById("submit-btn")
+
+// varible for the span containing how the user has rated.
+
+const span = document.getElementById("span")
+
+
+
+
 let rateBtn = document.getElementsByClassName("num-divs")
 let rated = 0;
 
 for (let i=0; i<rateBtn.length; i++){
     rateBtn[i].addEventListener('click', ()=>{
       rated = rateBtn[i].value;
+      rateBtn[i].classList.add("button-clicked");
+      
     })
 }
-
-// variable for submit button
-const submitBtn = document.getElementById("submit-btn")
 
 
 
 
 function clicked(){
-    rate1.classList.add("button-clicked")
-    rating = rate1.value
-    console.log(rating);
+    
+    if(rated > 0){
+        window.location.href="succsess.html"
+    } else{
+        alert("rate chikala")
+    }
+    
+    
+   
 }
 
-let rating = 0;
-rate1.addEventListener('click', clicked );
+submitBtn.addEventListener('click', clicked );
