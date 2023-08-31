972254e100042ac4a0251c25bb415570c1514596
Frank Kafweku <frankkafweku84@gmail.com>
2023-08-31 14:46:58 +0200

    Fix rate  buttons

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -1,5 +1,11 @@
+// ratings
 const rate1 = document.getElementById("rate-1")
 const rate2 = document.getElementById("rate-2")
 const rate3 = document.getElementById("rate-3")
 const rate4 = document.getElementById("rate-4")
 const rate5 = document.getElementById("rate-5")
+
+// variable for submit button
+const submitBtn = document.getElementById("submit-btn")
+
+console.log(rate2.value);
