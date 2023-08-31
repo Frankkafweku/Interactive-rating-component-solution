78204c722eed7c987ddc2b8e724445bfb0c220ba
Frank Kafweku <frankkafweku84@gmail.com>
2023-08-31 17:40:55 +0200

    Use a for loop to add event listeners to the rate buttons

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -5,7 +5,26 @@ const rate3 = document.getElementById("rate-3")
 const rate4 = document.getElementById("rate-4")
 const rate5 = document.getElementById("rate-5")
 
+let rateBtn = document.getElementsByClassName("num-divs")
+let rated = 0;
+
+for (let i=0; i<rateBtn.length; i++){
+    rateBtn[i].addEventListener('click', ()=>{
+      rated = rateBtn[i].value;
+    })
+}
+
 // variable for submit button
 const submitBtn = document.getElementById("submit-btn")
 
-console.log(rate2.value);
+
+
+
+function clicked(){
+    rate1.classList.add("button-clicked")
+    rating = rate1.value
+    console.log(rating);
+}
+
+let rating = 0;
+rate1.addEventListener('click', clicked );
